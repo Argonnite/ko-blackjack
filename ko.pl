968834db0783740c2887b6c64e899a3b65a88ae3
Argonnite <dermalin3k@hotmail.com>
2016-01-12 18:32:29 +0000

    interim check-in

--- ko.pl
+++ ko.pl
@@ -3,8 +3,15 @@ use Data::Dumper;
 use Scalar::Util qw/looks_like_number/;
 
 
-###init bankroll
-my $playerChips = "100";
+my $nDecks = 8;
+my $cut;            # penetration
+my $nTrials;        # number of shoes to simulate
+my $spreadMin;      # limits on the betting spread
+my $spreadMax;
+my $RCmin;          # running min count reached for the shoe
+my $RCmax;          # running max count reached for the shoe
+my $rsLimit = 3;    # limit on resplits.  -1 if unlimited.
+my $spotsLimit = 2; # number of those seated
 
 
 
@@ -21,19 +28,16 @@ foreach my $rank (@ranks) {
 @deck = shuffle @deck;
 
 
-
-###place bets
-my $bet = 'a';
-while(not looks_like_number $bet) {
-  print "Enter bet (You have $playerChips):";
-  $bet = <STDIN>;
-}
-chomp $bet;
-$playerChips -= $bet;
-print "Betting $bet.\n";
-print "You have $playerChips remaining.\n";
-
-
+####place bets
+#my $bet = 'a';
+#while(not looks_like_number $bet) {
+#  print "Enter bet (You have $playerChips):";
+#  $bet = <STDIN>;
+#}
+#chomp $bet;
+#$playerChips -= $bet;
+#print "Betting $bet.\n";
+#print "You have $playerChips remaining.\n";
 
 
 ###dealer's cards
@@ -42,89 +46,169 @@ push @dealer,pop @deck;
 push @dealer,pop @deck;
 print "Dealer: " . join(' ',("XX"),@dealer[1]) . "\n";
 
-
-
-
-###player's cards
-my @cards;
-push @cards,pop @deck;
-push @cards,pop @deck;
-
-
-
-###init table spot
+###players' cards
 my @places;
-my $pos = 0;
-push @places,{('bet' => $bet, 'cards' => \@cards, 'pos' => $pos)};
-#print Dumper(\@places);
-#print scalar @deck . " cards remaining.\n";
+my @cards;
+my $bet = 1;
+for(my $i = 0; $i < $spotsLimit; ++$i) {
+    push @places,{('bet' => $bet, 'cards' => [pop @deck, pop @deck], 'pos' => $i, 'splits' => 0)};
+}
+
+
+
+#
+####testes
+#my @blah = ('as','6s','th','as');
+#print Dumper(\@blah);
+#@blah = getTotals(\@blah);
+#print Dumper(\@blah);
+#exit(0);
+#
+#
+#
 
 
 
 my @patPlaces;
-
 while (scalar @places) {
-  my $hand = pop @places;
-  print Dumper($hand);
-  my $key = "h";
-  while ($key eq "h" or $key eq "p") {
+    my $hand = pop @places;
+    print Dumper($hand);
+#    print Dumper(\@places);
     print scalar @deck . " cards remaining.\n";
-    print "Player: " . join(' ',@{$hand->{cards}}) . "\n";
-    print "(h)it, (s)tand, (d)ouble down, s(p)lit:\n";
-    $key = <>;
-    chomp $key;
-    if($key eq "h") {
-      print "Hitting.\n";
-      push @{$hand->{cards}},pop @deck;
-      if(busted($hand)) {
-	$key = '';
-	$hand->{bet} = 0;
-	push @patPlaces,$hand;
-	undef $hand;
-      }
-    } elsif ($key eq "s") {
-      print "Standing.\n";
-      push @patPlaces,$hand;
-      undef $hand;
-    } elsif ($key eq "d") {
-      print "Doubling-down.\n";
-      $playerChips -= $hand->{bet}; # betting more chips.
-      print "You have $playerChips remaining.\n";
-      push @{$hand->{cards}},pop @deck;
-      if(busted($hand)) {
-	$key = '';
-	$hand->{bet} = 0;
-	push @patPlaces,$hand;
-	undef $hand;
-      } else {
-	$hand->{bet} += $bet;
-      }
-      push @patPlaces,$hand;
-      undef $hand;
-    } elsif ($key eq "p") {
-      print "Splitting.\n";
-      my @newCards;
-      push @newCards,pop @{$hand->{cards}};
-      push @newCards,pop @deck;
-      push @{$hand->{cards}},pop @deck;
-      push @places,{('bet' => $bet, 'cards' => \@newCards, 'pos' => ++$pos)};
-      $playerChips -= $bet;
-      print "You have $playerChips remaining.\n";
-    } else {
-      print "Not a valid choice.\n";
-      $key = "h";
-    }
-  }
+    my @totals = getTotals($hand->{'cards'});
+    print Dumper(\@totals);
 }
 
+
+####sub _addIt
+#sub _addIt {
+#    my @cards = @{$_[0]};
+#    my @totals = @{$_[1]};
+#
+#    my $card = shift @cards;
+#
+#    my $rank = substr($card,0,1);
+#    print "Card is $card.\n";
+#    if($rank eq 'a') {
+#	print "It's an ace.\n";
+#    } elsif($rank eq 't') {
+#	print "It's a ten.\n";
+#    } elsif(!looks_like_number($rank)) {
+#	print "It's a ten.\n";
+#    } elsif(looks_like_number($rank)) {
+#	print "It's number.\n";
+#    } else {
+#	print "ERROR.\n";
+#    }
+#}
+
+
+###sub getTotals
+sub getTotals {
+    my $cards = shift(@_);
+    my @sums = (0);
+#    _addIt($cards,\@sums);
+
+    foreach my $card (@{$cards}) {
+	my @newSums = ();
+	my $rank = substr($card,0,1);
+	print "Card is $card.\n";
+	if($rank eq 'a') {
+	    print "It's an ace.\n";
+	    foreach my $total (@sums) {  ### add ones
+		push @newSums,($total + 1);
+	    }
+	    foreach my $total (@sums) {  ### add elevens
+		push @newSums,($total + 11);
+	    }
+	} elsif($rank eq 't') {
+	    print "It's a ten.\n";
+	    foreach my $total (@sums) {
+		push @newSums,($total + 10);
+	    }
+	} elsif(!looks_like_number($rank)) {
+	    print "It's a ten.\n";
+	    foreach my $total (@sums) {
+		push @newSums,($total + 10);
+	    }
+	} elsif(looks_like_number($rank)) {
+	    print "It's number.\n";
+	    foreach my $total (@sums) {
+		push @newSums,($total + $rank);
+	    }
+	} else {
+	    print "ERROR.\n";
+	}
+#print Dumper(\@sums);
+	@sums = @newSums;
+#print Dumper(\@sums);
+#print "YEAH\n";
+    }
+    return @sums;
+}
+
+#    my $key = "h";
+#    while ($key eq "h" or $key eq "p") {
+#	print scalar @deck . " cards remaining.\n";
+#	print "Player: " . join(' ',@{$hand->{cards}}) . "\n";
+#	print "(h)it, (s)tand, (d)ouble down, s(p)lit:\n";
+#	$key = <>;
+#	chomp $key;
+#	if($key eq "h") {
+#	    print "Hitting.\n";
+#	    push @{$hand->{cards}},pop @deck;
+#	    if(busted($hand)) {
+#		$key = '';
+#		$hand->{bet} = 0;
+#		push @patPlaces,$hand;
+#		undef $hand;
+#	    }
+#	} elsif ($key eq "s") {
+#	    print "Standing.\n";
+#	    push @patPlaces,$hand;
+#	    undef $hand;
+#	} elsif ($key eq "d") {
+#	    print "Doubling-down.\n";
+#	    $playerChips -= $hand->{bet}; # betting more chips.
+#	    print "You have $playerChips remaining.\n";
+#	    push @{$hand->{cards}},pop @deck;
+#	    if(busted($hand)) {
+#		$key = '';
+#		$hand->{bet} = 0;
+#		push @patPlaces,$hand;
+#		undef $hand;
+#	    } else {
+#		$hand->{bet} += $bet;
+#	    }
+#	    push @patPlaces,$hand;
+#	    undef $hand;
+#	} elsif ($key eq "p") {
+#	    print "Splitting.\n";
+#	    my @newCards;
+#	    push @newCards,pop @{$hand->{cards}};
+#	    push @newCards,pop @deck;
+#	    push @{$hand->{cards}},pop @deck;
+#	    push @places,{('bet' => $bet, 'cards' => \@newCards, 'pos' => ++$pos)};
+#	    $playerChips -= $bet;
+#	    print "You have $playerChips remaining.\n";
+#	} else {
+#	    print "Not a valid choice.\n";
+#	    $key = "h";
+#	}
+#    }
+#}
+    
 
 print "BLAHHHHH\n";
 print Dumper(\@patPlaces);
 
 
-###sub busted
-###sub soft
-###sub total
+
+
+
+###sub isBusted
+###sub isSoft
+###sub isPair
 
 ####dealer's turn
 #while(total(@dealer) < 17 and soft(@dealer) and not busted(@dealer)) {
