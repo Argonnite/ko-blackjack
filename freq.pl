e747dd67d6767fcb17030295e9addaf08102f076
Argonnite <dermalin3k@hotmail.com>
2016-02-03 10:19:59 +0000

    hack

--- freq.pl
+++ freq.pl
@@ -1,14 +1,16 @@
 use strict;
 use warnings;
+use Data::Dumper;
 
 my $filename = 'blah.csv';
 
 open(my $fh, '<', $filename) or die "Can't open $!";
 
 my %hist;
+my $n = 0;
+my $nSevens = 0;
 
 while(<$fh>) {
-#  print;
   chomp;
 
   my $raw = $_;
@@ -30,12 +32,39 @@ while(<$fh>) {
   if($raw == 64) { $raw = 46; }
   if($raw == 65) { $raw = 56; }
 
-  $hist{$raw}++;
+  if($raw == 11) { $hist{'11'}++; }
+  if($raw == 12) { $hist{'12'}++; }
+  if($raw == 13) { $hist{'13'}++; }
+  if($raw == 14) { $hist{'14'}++; }
+  if($raw == 15) { $hist{'15'}++; }
+  if($raw == 16) { $hist{'16'}++; $nSevens++; }
+  if($raw == 22) { $hist{'22'}++; }
+  if($raw == 23) { $hist{'23'}++; }
+  if($raw == 24) { $hist{'24'}++; }
+  if($raw == 25) { $hist{'25'}++; $nSevens++; }
+  if($raw == 26) { $hist{'26'}++; }
+  if($raw == 33) { $hist{'33'}++; }
+  if($raw == 34) { $hist{'34'}++; $nSevens++; }
+  if($raw == 35) { $hist{'35'}++; }
+  if($raw == 36) { $hist{'36'}++; }
+  if($raw == 44) { $hist{'44'}++; }
+  if($raw == 45) { $hist{'45'}++; }
+  if($raw == 46) { $hist{'46'}++; }
+  if($raw == 55) { $hist{'55'}++; }
+  if($raw == 56) { $hist{'56'}++; }
+  if($raw == 66) { $hist{'66'}++; }
 
+  ++$n;
 
 }
 
-foreach my $key (sort(keys %hist)) {
-  print $key . " - " . $hist{$key} . "\n";
-}
+#while ((my $key, my $value) = each %hist) {
+#  print $key . " - " . $value . "\n";
+#}
+#
+print Dumper(\%hist);
 
+
+print "SEVENS = $nSevens\n";
+print "N = $n\n";
+print "1:" . $n/$nSevens . "\n";
